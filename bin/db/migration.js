62e768a0579283302803d2b237d390dc870599a0
Ubuntu <ubuntu@ip-10-179-160-38.ec2.internal>
2017-12-14 20:14:39 +0000

    fixed migration

--- bin/db/migration.js
+++ bin/db/migration.js
@@ -3,16 +3,16 @@ const user = require('./user.json');
 const knex = require('knex')({
 	dialect: 'sqlite3',
 	connection: {
-		filename: './bin/db/data.db'
+		filename: './data.db'
 	},
 	useNullAsDefault: true
 });
 
-// knex.schema.createTable('users', function(table) {
-//   table.increments('id');
-//   table.string('user_name');
-//   table.string('password');
-// }).then((res) => {})
+ knex.schema.createTable('users', function(table) {
+   table.increments('id');
+   table.string('user_name');
+   table.string('password');
+ }).then((res) => {})
 
 bcrypt.genSalt(10, function(err, salt) {
 	bcrypt.hash(user.password, salt, function(err, hash) {
