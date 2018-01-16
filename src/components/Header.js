6e89c511f3128528b759c24073c76105e13f35c0
Dillon <dmcguire@emmisdigital.com>
2018-01-16 15:19:13 -0600

    added upload to path on logout link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
 		return (
 			<header>
 				<img src="http://base.emmis.acsitefactory.com/sites/all/modules/custom/emmis_theme/logos/base.png" />
-				<a className="logout" href="/login">
+				<a className="logout" href="/upload/login">
 					log out
 				</a>
 			</header>
