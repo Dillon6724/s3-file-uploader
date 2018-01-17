11c381e630692e9d3d03be5c115e22e001713714
Dillon <dmcguire@emmisdigital.com>
2018-01-17 11:52:04 -0600

    updated server routes naming convention and cors

--- bin/server.js
+++ bin/server.js
@@ -7,28 +7,33 @@ const multipartMiddleware = multipart();
 const cors = require('cors');
 const config = require('../config');
 
+var corsOptions = {
+	origin: 'http://emmisdigital.com',
+	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
-app.post('/upload', multipartMiddleware, (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+app.post('/upload/upload', multipartMiddleware, (req, res) => {
+	// res.setHeader('Access-Control-Allow-Origin', '*');
 	controller.upload(req.files.imageFile, res);
 });
 
-app.post('/login', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+app.post('/upload/login', (req, res) => {
+	// res.setHeader('Access-Control-Allow-Origin', '*');
 	controller.login(req, res);
 });
 
-app.get('/files', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+app.get('/upload/files', (req, res) => {
+	// res.setHeader('Access-Control-Allow-Origin', '*');
 	controller.getFiles(res);
 });
 
-app.get('/', (req, res) => {
+app.get('/upload', (req, res) => {
 	res.send('Lemme lemme upload ya');
 });
 
