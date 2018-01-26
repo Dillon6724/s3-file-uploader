01abe44c7b36c71483a02f8881af96942eb2c424
Dillon <dmmcguire6724@gmail.com>
2018-01-26 15:18:16 -0600

    back to npm cors after moving to same domain

--- bin/server.js
+++ bin/server.js
@@ -7,38 +7,22 @@ const multipartMiddleware = multipart();
 const cors = require('cors');
 const config = require('../config');
 
-// var corsOptions = {
-// 	origin: 'http://upload.dillon-mcguire.com',
-// 	optionsSuccessStatus: 200
-// };
-//
-// // middleware
-// app.options('*', cors());
-// app.use(cors(corsOptions));
+var corsOptions = {
+	origin: 'http://upload.dillon-mcguire.com',
+	optionsSuccessStatus: 200
+};
+
+// middleware
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'http://upload.dillon-mcguire.com');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
-
 // routes
 app.post('/upload', multipartMiddleware, (req, res) => {
-	// if (req.method === 'OPTIONS') {
-	// 	return res.status(200).end();
-	// }
 	controller.upload(req.files.imageFile, res);
 });
 
 app.post('/login', (req, res) => {
-	// if (req.method === 'OPTIONS') {
-	// 	return res.status(200).end();
-	// }
 	controller.login(req, res);
 });
 
